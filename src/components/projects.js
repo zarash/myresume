7f742bba33132643fafaeebdbddd04b76919dd3c
zarash <z.ashrafi@yahoo.com>
2019-10-09 11:41:57 +0330

    edit project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle,CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import { Tabs, Tab, Card, CardTitle,CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component{
     constructor(props){
@@ -14,48 +14,33 @@ class Projects extends Component{
                     <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: 'grean', height: '176px', background: 'url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png) center / cover'}}> React Project #1</CardTitle>
                     <CardText>
-                    Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+                    This is React project.
                     </CardText>
                      <CardActions border>
                         <Button colored>Github</Button>
                         <Button colored>CodePen</Button>
                         <Button colored>LiveDemo</Button>
                     </CardActions>
-                    <CardMenu style={{ color: '#fff'}}>
+                    <CardMenu style={{ color: '#000'}}>
                         <IconButton name="share" />
                     </CardMenu>
                     </Card>
-
-                    {/* this is 2 project */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: 'grean', height: '176px', background: 'url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png) center / cover'}}> React Project #2</CardTitle>
-                    <CardText>
-                    Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
-                    </CardText>
-                     <CardActions border>
-                        <Button colored>Github</Button>
-                        <Button colored>CodePen</Button>
-                        <Button colored>LiveDemo</Button>
-                    </CardActions>
-                    <CardMenu style={{ color: '#fff'}}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                    </Card>
-
-                </div>
-                
+                </div>            
             )
         } else if (this.state.activeTab ===1 ){
             return (  
-                <div><h1>This is</h1></div>
-            )
-        }else if (this.state.activeTab ===2 ){
-            return (
-                <div><h1>This is</h1></div>
-            )
-        }else if (this.state.activeTab ===3 ){
-            return (
-                <div><h1>This is</h1></div>
+                <Card shadow={5} style={{minWidth:'450' , margin: 'auto'}}>
+                    <CardTitle style={{color:'grean', height:'176px', background:'url(http://www.sitepoint.com/wp-content/themes/sitepoint/assets/images/icon.ruby.png) center/cover'}}>Ruby on Rails Project #1</CardTitle>
+                    <CardText>This is Ruby on Rails Project.</CardText>
+                    <CardActions>
+                        <Button colored>Github</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: '#000'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
             )
         }
     }
@@ -66,17 +51,11 @@ class Projects extends Component{
                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple >
                    <Tab>React</Tab>
                    <Tab>Ruby on Rails</Tab>
-                   <Tab>PHP</Tab>
                </Tabs>
 
-               
-                   <Grid>
-                       <Cell col={12}>
-                           <div className="content">{this.toggleCategories()}</div>
-                       </Cell>
-                   </Grid>
-                   {this.toggleCategories()}
-               
+            <section className="project-grid">
+                {this.toggleCategories()}
+            </section>
            </div> 
         )
     }
